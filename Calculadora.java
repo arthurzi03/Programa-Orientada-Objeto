f7c630f61d4155038f309c3d64200ea2dae1fcc8
Arthur Oliveira <129237472+arthurzi03@users.noreply.github.com>
2024-03-19 17:27:50 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,21 @@
+package aula;
+
+public class Calculadora {
+    
+    double numA;
+    double numB;
+
+    double somar(){
+        return numA+numB;
+    }
+    double subtrair(){
+        return numA-numB;
+    }
+    double multiplicar(){
+        return numA*numB;
+    }
+    double dividir(){
+        return numA/numB;
+    }
+
+}
---
+++ arthur.java
@@ -0,0 +1,14 @@
+package aula;
+public class arthur {
+
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+        calc.numA=2.0;
+        calc.numB=3.0;
+        System.out.println("soma: " + calc.somar());
+        System.out.println("subtrair: " + calc.subtrair());
+        System.out.println("multiplicar: " + calc.multiplicar());
+        System.out.println("dividir: " + calc.dividir());
+    }
+}
